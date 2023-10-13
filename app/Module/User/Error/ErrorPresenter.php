eb0c2a643c8cf57584ba79996169e0078eccc82b
Chrynn <filipmachala88@gmail.com>
2023-10-13 16:57:54 +0200

    menu route created

--- app/Module/User/Error/ErrorPresenter.php
+++ app/Module/User/Error/ErrorPresenter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Presenters;
+namespace App\Module\User\Error;
 
 use Nette;
 use Nette\Application\Responses;
@@ -31,7 +31,7 @@ final class ErrorPresenter implements Nette\Application\IPresenter
 		$this->logger->log($exception, ILogger::EXCEPTION);
 		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
 			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
-				require __DIR__ . '/templates/Error/500.phtml';
+				require __DIR__ . '/templates/500.phtml';
 			}
 		});
 	}
---
+++ app/Module/User/Menu/MenuPresenter.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Module\User\Menu;
+
+use App\Module\User\UserPresenter;
+
+class MenuPresenter extends UserPresenter
+{
+	public function actionDefault(): void
+	{
+
+	}
+}
